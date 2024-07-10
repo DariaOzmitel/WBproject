a834bbc10a5c69005f0311e1b25ccd32a1bc3940
Daria <vvcxcv@mail.ru>
2024-07-10 18:26:11 +0300

    add CustomPin. CustomPhoneNumber

---
+++ app/src/main/java/com/example/wbproject/ui/theme/arguments/DropdownMenuItems.kt
@@ -0,0 +1,11 @@
+package com.example.wbproject.ui.theme.arguments
+
+import com.example.wbproject.R
+
+enum class DropdownMenuItems(
+    val imageResId: Int,
+    val countryCode: String
+) {
+    RUSSIA(R.drawable.russian_flag, "+7"),
+    SOUTH_KOREA(R.drawable.south_korea_flag, "+82")
+}
