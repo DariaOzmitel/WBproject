489b3d85e8f6836b494f7dace6310c4687243938
Daria <vvcxcv@mail.ru>
2024-08-03 13:19:12 +0300

    refactoring (add ChangeAttendingStatusUseCase, possibility of adding Avatar)

--- domain/src/test/java/com/example/domain/repository/MeetingRepositoryStub.kt
+++ domain/src/test/java/com/example/domain/repository/MeetingRepositoryStub.kt
@@ -3,6 +3,7 @@ package com.example.domain.repository
 import com.example.domain.mockData.mockListMeetingAlreadyPassed
 import com.example.domain.mockData.mockListMeetings
 import com.example.domain.model.Meeting
+import com.example.domain.model.User
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
@@ -19,4 +20,7 @@ internal class MeetingRepositoryStub : MeetingRepository {
         return flowOf(mockListMeetings.first { it.id == meetingId })
     }
 
+    override fun changeAttendingStatus(meetingId: Int, user: User) {
+        TODO("Not yet implemented")
+    }
 }
